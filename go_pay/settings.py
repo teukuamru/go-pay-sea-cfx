c70545629a6fa7b732769d0466034e02e61ab244
teukuamru <teukuamrullahfaisal@gmail.com>
2018-08-14 12:06:15 +0700

    fix whitenoise

--- go_pay/settings.py
+++ go_pay/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
